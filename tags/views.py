aa12272cd8426a179e2cb8cda535cd794afe6915
Olle Brask <ollstah@gmail.com>
2024-06-27 09:58:32 +0000

    Add generic views for tags

--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import permissions
+from .models import Tag
+from .serializers import TagSerializer
 
-# Create your views here.
+class TagList(generics.ListCreateAPIView):
+    """
+    List tags or create a new tag.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class TagDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, or delete a tag.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
