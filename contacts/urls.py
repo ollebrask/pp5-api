42969078572d24a9bceb46c9a4c7d9b5ed831f23
Olle Brask <ollstah@gmail.com>
2024-06-27 11:25:19 +0000

    Update urls for contacts

---
+++ contacts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from contacts import views
+
+urlpatterns = [
+    path('contacts/', views.ContactListView.as_view(), name='contact-list'),
+    path('contacts/<int:pk>/', views.ContactDetailView.as_view(), name='contact-detail'),
+    path('contact/', views.ContactCreateView.as_view(), name='contact-create'),
+]
--- pp5_api/urls.py
+++ pp5_api/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', include('likes.urls')),
     path('', include('followers.urls')),
     path('', include('tags.urls')),
+    path('', include('contacts.urls')),
 ]
