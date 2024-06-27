d880a1f021ebf4359fa29939041b0ab0bb9506bc
Olle Brask <ollstah@gmail.com>
2024-06-27 09:51:47 +0000

    Add validation to tag model

--- tags/models.py
+++ tags/models.py
@@ -1,10 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+import re
+
+# found at:
+# https://stackoverflow.com/questions/16982625
+# /only-accept-alphanumeric-characters-and-underscores-for-a-string-in-python
+def validate_tag_name(value):
+    if not re.match(r'^[a-zA-Z0-9_]+$', value):
+        raise ValidationError(
+            'Tag name can only be alphanumeric characters and underscores.')
+
 
 class Tag(models.Model):
     """
     Tag model for hashtags.
     """
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(
+        max_length=100, 
+        unique=True, 
+        validators=[validate_tag_name],
+        null=False
+    )
 
     class Meta:
         ordering = ['name']
