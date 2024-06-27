c22c67de0a6820349b72d49016ec7371788e6ff2
Olle Brask <ollstah@gmail.com>
2024-06-27 11:24:55 +0000

    Add views for contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Contact
+from .serializers import ContactListSerializer, ContactDetailSerializer, ContactSerializer
+from .permissions import IsSuperUser
 
-# Create your views here.
+class ContactCreateView(generics.CreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+class ContactListView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactListSerializer
+    permission_classes = [IsSuperUser]
+
+class ContactDetailView(generics.RetrieveAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactDetailSerializer
+    permission_classes = [IsSuperUser]
