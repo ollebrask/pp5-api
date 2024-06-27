d8c16de9817fce5e2c6bdb0b2c2fb08d166de969
Olle Brask <ollstah@gmail.com>
2024-06-27 12:01:53 +0000

    Add search and filters for contacts view

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Contact
 from .serializers import ContactListSerializer, ContactDetailSerializer, ContactSerializer
 from .permissions import IsSuperUser
@@ -11,6 +12,14 @@ class ContactListView(generics.ListAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactListSerializer
     permission_classes = [IsSuperUser]
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = ['email', 'subject', 'created_at']
+    search_fields = ['name', 'email', 'subject', 'message']
+    ordering_fields = ['created_at']
 
 class ContactDetailView(generics.RetrieveAPIView):
     queryset = Contact.objects.all()
