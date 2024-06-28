50b8f2a8f4b7c1830085a34b986b573fea8927f2
Olle Brask <ollstah@gmail.com>
2024-06-28 09:48:32 +0000

    Edit allowed hosts

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -60,7 +60,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-ollebrask-pp5api-k4gox9bxmr6.ws-eu114.gitpod.io', 'pp5-olle-4b42abf5cfb4.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ollebrask-pp5api-k4gox9bxmr6.ws-eu114.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -199,5 +199,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CSRF settings
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-ollebrask-pp5api-k4gox9bxmr6.ws-eu114.gitpod.io'
+    'https://8000-ollebrask-pp5api-k4gox9bxmr6.ws-eu114.gitpod.io',
+    '.herokuapp.com'
 ]
